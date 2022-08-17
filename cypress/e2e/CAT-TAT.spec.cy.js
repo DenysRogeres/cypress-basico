2d37f2bdb1cee71df04e7b8f81db1681bbf7ead4
Denys Rógeres <dennysrogeres@gmail.com>
2022-08-17 14:10:00 -0300

    adicionando novos testes

--- cypress/e2e/CAT-TAT.spec.cy.js
+++ cypress/e2e/CAT-TAT.spec.cy.js
@@ -9,7 +9,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
   });
 
-  it.only('preenche os campos obrigatórios e envia o formulário', () => {
+  it('preenche os campos obrigatórios e envia o formulário', () => {
     cy.clock();
 
     cy.get('#firstName').type('Denys')
@@ -171,9 +171,47 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.get('#privacy a').should('have.attr', 'target', '_blank')
   })
 
-  it('acessa a página da política de privacidade removendo o target e então clicanco no link', () => {
-    cy.get('#privacy a').invoke('removeAttr', 'target').click()
-    cy.get('#title').should('be.visible', 'CAT TAT - Política de privacidade')
-    cy.contains('Talking About Testing').should('be.visible')
+  Cypress._.times(5, () => {
+    it('acessa a página da política de privacidade removendo o target e então clicanco no link', () => {
+        cy.get('#privacy a').invoke('removeAttr', 'target').click()
+        cy.get('#title').should('be.visible', 'CAT TAT - Política de privacidade')
+        cy.contains('Talking About Testing').should('be.visible')
+      })
+  })
+
+  it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+    cy.get('.success')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Mensagem enviada com sucesso.')
+      .invoke('hide')
+      .should('not.be.visible')
+      
+    cy.get('.error')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Valide os campos obrigatórios!')
+      .invoke('hide')
+      .should('not.be.visible')
+  });
+
+  it('preenche a area de texto usando o comando invoke', () => {
+    const longText = Cypress._.repeat('123456879', 20);
+
+    cy.get('#open-text-area')
+      .invoke('val', longText)
+      .should('have.value', longText);
+  });
+
+  it.only('Faz uma requisição HTTP', () => {
+    cy.request('https://pt.wikipedia.org/wiki/REST')
+      .should(function(response){
+        const { status, statusText, body } = response;
+
+        expect(status).to.equal(200)
+        expect(statusText).to.equal('OK')
+      })
   })
 })
